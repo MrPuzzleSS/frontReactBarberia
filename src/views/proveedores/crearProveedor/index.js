d8c4acaf570fc815e53f8c4c052ee71025b3ed32
Puzzless <simon01ruiz@gmail.com>
2023-11-20 19:59:18 -0500

    CRUD proveedores y servicio http

--- src/views/proveedores/crearProveedor/index.js
+++ src/views/proveedores/crearProveedor/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import ProveedoresDataService from 'src/views/services/ProveedoresService';
+import Swal from 'sweetalert2'
 import {
   CButton,
   CCard,
@@ -11,9 +13,45 @@ import {
   CFormInput,
   CFormLabel,
   CRow,
-} from '@coreui/react'
+} from '@coreui/react';
 
-function CrearProveedor() {
+const CrearProveedor = () => {
+  const initialProveedorState = {
+    nombre: '',
+    direccion: '',
+    telefono: '',
+    email: '',
+    tipo_de_producto_servicio: '',
+  };
+
+  const [proveedor, setProveedor] = useState(initialProveedorState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setProveedor({ ...proveedor, [name]: value });
+  };
+
+  const saveProveedor = () => {
+    ProveedoresDataService.create(proveedor)
+      .then((response) => {
+        setProveedor(response.data);
+        setSubmitted(true);
+        console.log(response.data);
+
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Proveedor Creado Exitosamente",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      })
+      .catch((error) => {
+        console.error('Error al crear proveedor:', error);
+      });
+  };
+
   return (
     <CRow>
       <CCol xs={12}>
@@ -22,40 +60,69 @@ function CrearProveedor() {
             <strong>Crear Proveedor</strong>
           </CCardHeader>
           <CCardBody>
-            <CForm className="row gy-4 gx-3 align-items-center">
+            <CForm>
               <CCol sm={6}>
                 <CFormLabel>Nombre del Proveedor</CFormLabel>
-                <CFormInput type="text" />
+                <CFormInput
+                  type='text'
+                  name='nombre'
+                  value={proveedor.nombre}
+                  onChange={handleInputChange}
+                />
               </CCol>
-                <CCol sm={6}>
+              <CCol sm={6}>
                 <CFormLabel>Dirección</CFormLabel>
-                <CFormInput />
+                <CFormInput
+                  type='text'
+                  name='direccion'
+                  value={proveedor.direccion}
+                  onChange={handleInputChange}
+                />
               </CCol>
-                <CCol sm={6}>
+              <CCol sm={6}>
                 <CFormLabel>Teléfono</CFormLabel>
-                <CFormInput type="tel" />
+                <CFormInput
+                  type='tel'
+                  name='telefono'
+                  value={proveedor.telefono}
+                  onChange={handleInputChange}
+                />
               </CCol>
-              <CCol sm={6}>  
+              <CCol sm={6}>
                 <CFormLabel>Correo Electrónico</CFormLabel>
-                <CFormInput />
+                <CFormInput
+                  type='email'
+                  name='email'
+                  value={proveedor.email}
+                  onChange={handleInputChange}
+                />
               </CCol>
               <CCol sm={6}>
                 <CFormLabel>Producto/Servicio</CFormLabel>
-                <CFormInput />
+                <CFormInput
+                  type='text'
+                  name='tipo_de_producto_servicio'
+                  value={proveedor.tipo_de_producto_servicio}
+                  onChange={handleInputChange}
+                />
               </CCol>
-              <CCol xs={12}>
-              <Link to="/proveedores/lista-proveedores">
-              <CButton type="submit" color="primary">
+              <CButton type='button' color='primary' onClick={saveProveedor} to='/proveedores/lista-proveedores'>
                 Crear Proveedor
               </CButton>
+              <Link to='/proveedores/lista-proveedores'>
+                <CButton type='button' color='secondary'>
+                  Cancelar
+                </CButton>
               </Link>
-              </CCol>
             </CForm>
+            {submitted && (
+              <Navigate to='/proveedores/lista-proveedores'/>
+            )}
           </CCardBody>
         </CCard>
       </CCol>
     </CRow>
-  )
-}
+  );
+};
 
-export default CrearProveedor
+export default CrearProveedor;
