7a031615399a213cb72216801d24ff54c70f0a6c
feliciano389 <felixx-21@hotmail.com>
2024-03-30 23:31:17 -0500

    cambios ultimos ventas

--- src/views/ventas/listaVentas/index.js
+++ src/views/ventas/listaVentas/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Swal from 'sweetalert2';
 import { faEye, faCheckCircle, faBan, faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons';
 import {
   CCard,
@@ -44,6 +45,8 @@ function ListaVentas() {
   const [searchTerm, setSearchTerm] = useState('');
   const [montoAbono, setMontoAbono] = useState(0);
   const [selectedVenta, setSelectedVenta] = useState(null);
+  const [abonosRealizados, setAbonosRealizados] = useState([]);
+
 
   useEffect(() => {
     fetchVentas();
@@ -68,8 +71,18 @@ function ListaVentas() {
     try {
       const data = await VentaService.getVentas();
       if (data && data.ventas) {
-        console.log(data.ventas);
-        setVentas(data.ventas);
+        const ventasWithAbonos = data.ventas.map(venta => {
+          const abono = abonos.find(abono => abono.id_ventas === venta.id_ventas);
+          if (abono) {
+            venta.valor_abonado = abono.precio_agregar;
+            venta.fecha_abono = abono.fecha_abono;
+          } else {
+            venta.valor_abonado = 0;
+            venta.fecha_abono = null;
+          }
+          return venta;
+        });
+        setVentas(ventasWithAbonos);
       } else {
         console.error('La respuesta de la API no contiene la propiedad "ventas":', data);
       }
@@ -91,6 +104,15 @@ function ListaVentas() {
     }
   };
 
+  // Función para agregar un nuevo abono
+  const agregarAbono = () => {
+    if (montoAbono > 0) {
+      const nuevoAbono = { monto: montoAbono, fecha: new Date() };
+      setAbonosRealizados([...abonosRealizados, nuevoAbono]);
+      setMontoAbono(0); // Reiniciar el campo de monto de abono
+    }
+  };
+
   const CambioAnulado = async (id_ventas) => {
     try {
       await VentaService.cambiarEstado(id_ventas);
@@ -108,14 +130,48 @@ function ListaVentas() {
 
   const handleSubmitAbono = async () => {
     try {
-      const response = await AbonoService.postAbonos(selectedVenta.id_ventas, montoAbono);
-      console.log("La respuesta: ", response); // Handle successful response from API
+      const fechaAbono = new Date();
+      const idCliente = selectedVenta.id_cliente;
+  
+      await AbonoService.postAbonos(selectedVenta.id_ventas, idCliente, montoAbono, fechaAbono);
+  
+      // Actualizar el estado de la lista de ventas después de un abono exitoso
+      const updatedVentas = ventas.map(venta => {
+        if (venta.id_ventas === selectedVenta.id_ventas) {
+          venta.valor_abonado += montoAbono;
+          venta.fecha_abono = fechaAbono;
+  
+          // Verificar si el valor abonado es igual al valor total de la venta
+          if (venta.valor_abonado === venta.precio) {
+            venta.estado = 'Pagado';
+          }
+        }
+        return venta;
+      });
+  
+      setVentas(updatedVentas);
+  
+      // Ocultar el modal después de realizar el abono
       setModalVisible(false);
-      fetchVentas(); // Refresh the list of ventas after successful abono
+  
+      // Mostrar mensaje de éxito con SweetAlert2
+      Swal.fire({
+        icon: 'success',
+        title: '¡Abono realizado con éxito!',
+        text: 'El abono se ha registrado correctamente.',
+      });
     } catch (error) {
       console.error('Error al enviar el abono:', error);
     }
   };
+
+
+
+
+
+
+
+
 
   function getColorForEstado(estado_anulado) {
     if (estado_anulado === 'Activo') {
@@ -187,8 +243,14 @@ function ListaVentas() {
                   {/* <CTableHeaderCell scope="col">#</CTableHeaderCell> */}
                   <CTableHeaderCell scope="col">Cliente</CTableHeaderCell>
                   <CTableHeaderCell scope="col">Empleado</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Nro° Factura</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">N° Factura</CTableHeaderCell>
                   <CTableHeaderCell scope="col">Valor Total</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Valor Abonado</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Deuda Pendiente</CTableHeaderCell>
+
+                  <CTableHeaderCell scope="col">Fecha Abono</CTableHeaderCell>
+
+
                   <CTableHeaderCell scope="col">Estado</CTableHeaderCell>
                   <CTableHeaderCell scope="col">Anular</CTableHeaderCell>
                   <CTableHeaderCell scope="col"></CTableHeaderCell>
@@ -202,13 +264,19 @@ function ListaVentas() {
                     <CTableDataCell>{venta.nombre_empleado}</CTableDataCell>
                     <CTableDataCell>{venta.numeroFactura}</CTableDataCell>
                     <CTableDataCell>
-                    {venta.precio.toLocaleString("es-CO", {
+                      {venta.precio.toLocaleString("es-CO", {
                         style: "currency",
                         currency: "COP",
                         minimumFractionDigits: 2,
                         maximumFractionDigits: 2,
-                    })}
+                      })}
                     </CTableDataCell>
+                    <CTableDataCell>{venta.valor_abonado}</CTableDataCell>
+                    <CTableDataCell>{venta.precio - venta.valor_abonado}</CTableDataCell>
+
+
+                    <CTableDataCell>{venta.fecha_abono ? new Date(venta.fecha_abono).toLocaleDateString() : '-'}</CTableDataCell>
+
                     <CTableDataCell>{venta.estado}</CTableDataCell>
                     <CTableDataCell><CBadge color={getColorForEstado(venta.estado_anulado)}>{venta.estado_anulado}</CBadge></CTableDataCell>
                     <CTableDataCell>
@@ -241,28 +309,28 @@ function ListaVentas() {
 
                         <div style={{ width: '10px' }} />
                         {venta.estado.toLowerCase() !== 'pagado' && (
-                        <CButton
-                          style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          padding: '8px 12px',
-                          border: '1px solid #28a745',
-                          borderRadius: '4px',
-                          backgroundColor: 'rgba(0, 255, 0, 0.2)',
-                          color: '#17a2b8',
-                          fontSize: '14px',
-                          textTransform: 'uppercase',
-                          cursor: 'pointer',
-                        }}
-                        size="sm"
-                        onClick={() => {
-                          setModalVisible(!modalVisible);
-                          handleAbono(venta);
-                        }}
-                        >
-                          <FontAwesomeIcon icon={faHandHoldingDollar} style={{ color: '#000000' }}/>
-                        </CButton>
+                          <CButton
+                            style={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              padding: '8px 12px',
+                              border: '1px solid #28a745',
+                              borderRadius: '4px',
+                              backgroundColor: 'rgba(0, 255, 0, 0.2)',
+                              color: '#17a2b8',
+                              fontSize: '14px',
+                              textTransform: 'uppercase',
+                              cursor: 'pointer',
+                            }}
+                            size="sm"
+                            onClick={() => {
+                              setModalVisible(!modalVisible);
+                              handleAbono(venta);
+                            }}
+                          >
+                            <FontAwesomeIcon icon={faHandHoldingDollar} style={{ color: '#000000' }} />
+                          </CButton>
                         )}
 
 
@@ -292,91 +360,85 @@ function ListaVentas() {
                 ))}
               </CTableBody>
             </CTable>
-            
+
             <CModal visible={visible} onClose={() => setVisible(false)} backdrop="static">
               <CModalHeader>
                 <CModalTitle>Detalle de Ventas</CModalTitle>
-                </CModalHeader>
-                <CModalBody style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  {detalleproductos && (
+              </CModalHeader>
+              <CModalBody style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                {detalleproductos && (
                   <div>
                     <h4>Detalles de Productos</h4>
                     <ul style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px' }}>
                       {detalleproductos.map((detalle, index) => (
-                      
-                      <li key={index} style={{
-                        listStyle: 'none',
-                        marginBottom: '10px',
-                        fontSize: '16px',
-                        flex: '0 0 auto',
-                        minWidth: '200px'
-                      }}>
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Nombre:</span> {detalle.nombre}, <br />
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Cantidad:</span> {detalle.cantidad}, <br />
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Precio Unitario:</span> {detalle.valor_venta}, <br />
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Valor Total:</span> {detalle.valor_total}
-                      </li>
-                    ))}
+
+                        <li key={index} style={{
+                          listStyle: 'none',
+                          marginBottom: '10px',
+                          fontSize: '16px',
+                          flex: '0 0 auto',
+                          minWidth: '200px'
+                        }}>
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Nombre:</span> {detalle.nombre}, <br />
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Cantidad:</span> {detalle.cantidad}, <br />
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Precio Unitario:</span> {detalle.valor_venta}, <br />
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Valor Total:</span> {detalle.valor_total}
+                        </li>
+                      ))}
                     </ul>
                   </div>
                 )}
-                <br/>
+                <br />
                 {detalleservicios && (
-                <div>
-                  <h4>Detalles de Servicios</h4>
-                  <ul style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px' }}>
-                    {detalleservicios.map((detalle, index) => (
-                    <li key={index} style={{
-                      listStyle: 'none',
-                      marginBottom: '10px',
-                      fontSize: '16px',
-                      flex: '0 0 auto',
-                      minWidth: '200px'
-                      }}>
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Nombre:</span>{detalle.nombre}, <br />
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Cantidad:</span>{detalle.cantidad}, <br />
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Precio Unitario:</span>{detalle.valor_venta}, <br />
-                        <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Valor Total:</span>{detalle.valor_total}
-                    </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </CModalBody>
-          </CModal>
-          
-          
-          <CModal visible={modalVisible} onClose={() => setModalVisible(false)} backdrop="static">
-            <CModalHeader>
-              <CModalTitle>Abonos</CModalTitle>
-            </CModalHeader>
-            <CModalBody>
-              <p>Precio a Pagar: {selectedVenta && selectedVenta.precio}</p>
-              <CInputGroup className="mb-3">
-                <CFormInput
-                type="number"
-                value={montoAbono}
-                onChange={(e) => setMontoAbono(e.target.value)}
-              />
-              </CInputGroup>
-              <CTable align='middle' className='mb-0 border' hover responsive>
-                <CTableHead>
-                  <CTableRow>
-                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Precio Agregar</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Fecha</CTableHeaderCell>
-                    </CTableRow>
-                    </CTableHead>
-                    <CTableBody>
+                  <div>
+                    <h4>Detalles de Servicios</h4>
+                    <ul style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px' }}>
+                      {detalleservicios.map((detalle, index) => (
+                        <li key={index} style={{
+                          listStyle: 'none',
+                          marginBottom: '10px',
+                          fontSize: '16px',
+                          flex: '0 0 auto',
+                          minWidth: '200px'
+                        }}>
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Nombre:</span>{detalle.nombre}, <br />
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Cantidad:</span>{detalle.cantidad}, <br />
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Precio Unitario:</span>{detalle.valor_venta}, <br />
+                          <span style={{ fontWeight: 'bold', marginRight: '5px' }}>Valor Total:</span>{detalle.valor_total}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </CModalBody>
+            </CModal>
 
-                    </CTableBody>
-                  </CTable>
-                  </CModalBody>
-                  <CModalFooter>
-                    <CButton color="primary" onClick={handleSubmitAbono}>Confirmar Abono</CButton>
-                    <CButton color="secondary" onClick={() => setModalVisible(false)}>Cancelar</CButton>
-                </CModalFooter>
-          </CModal>
+
+            <CModal visible={modalVisible} onClose={() => setModalVisible(false)} backdrop="static">
+              <CModalHeader>
+                <CModalTitle>Abonos</CModalTitle>
+              </CModalHeader>
+              <CModalBody>
+                <p>Deuda total: ${selectedVenta && (selectedVenta.precio - selectedVenta.valor_abonado)}</p>
+                <p>Valor Abonado: ${selectedVenta && selectedVenta.valor_abonado}</p>
+                <CInputGroup className="mb-3">
+                  <CFormInput
+                    type="number"
+                    placeholder="Ingrese el monto a abonar"
+                    value={montoAbono}
+                    onChange={(e) => setMontoAbono(parseFloat(e.target.value))}
+                  />
+                </CInputGroup>
+                <p>Fecha: {new Date().toLocaleDateString()}</p>
+              </CModalBody>
+              <CModalFooter>
+                <CButton color="primary" onClick={handleSubmitAbono}>Confirmar Abono</CButton>
+                <CButton color="secondary" onClick={() => setModalVisible(false)}>Cancelar</CButton>
+              </CModalFooter>
+            </CModal>
+
+
+
 
 
             <CPagination align="center" aria-label="Page navigation example" className="mt-3">
